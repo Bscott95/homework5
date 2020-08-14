94e4ea83c174418f35a528c4d9fade16d4d8f3aa
Brandon Scott <btfs95@gmail.com>
2020-08-14 15:04:54 -0400

    added more comments

--- Develop/app.js
+++ Develop/app.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     let startingHour = 9
     let endingHour = 17
 
-    // adds rows and hours in the day
+    // Funciton that takes in a starting hour and an ending hour and renders the contents of the page
     function renderRows(sHour,eHour){
         $('.container').html('')
         for (let hour = sHour; hour <= eHour; hour++) {
@@ -24,37 +24,29 @@ $(document).ready(function () {
             $hourRow.addClass('row')
             $('.container').append($hourRow)
 
-            // add the hour block
+            // add the hour block on the left hand side to the row div
             $hour = $('<p>')
             $hour.text(`${hour}:00`)
             $hourRow.append($hour)
             $hour.addClass('hour')
 
-            // add the text area block
+            // add the text area block next to the hour block
             $textArea = $('<textarea>')
             $hourRow.append($textArea)
             $textArea.attr('id', `text-${hour}`)
             // call function to color hours
             checkHourColor(hour)
 
-            // add the save button block
+            // add the save button block next to the text area block and the icons
             $saveBtn = $('<div>')
             $hourRow.append($saveBtn)
             $saveBtn.addClass('saveBtn')
             $saveBtn.attr('id', `save-${hour}`)
-            $saveBtn.html(`<i class="fa fa-floppy-o" aria-hidden="true"></i>`)
-
+            $saveBtn.html(`<i class="far fa-save fa-2x"></i>`)
         }
     }
 
-    // adding colors to hard-coded HTML elements seems like a pain
-    // for(let i=9;i<=17;i++){
-    //     if (moment().hour() > $('.hour')[i].textContent) {
-    //         $textArea.addClass('past')
-    //     }
-    // }
-
-    // function to color the text areas based on time
+    // function that takes in hour from our for loop and compares it to the moment hour. Colors the row accordingly.
     function checkHourColor(theHour) {
         if (moment().hour() > theHour) {
             $textArea.addClass('past')
@@ -74,11 +66,12 @@ $(document).ready(function () {
     // WHEN I refresh the page
     // THEN the saved events persist
 
+    // on clicking the save button, save the text in text area to the page and local storage. 
     $('.saveBtn').on('click', function (event) {
         // get the number row we are in
         let textNum = event.target.id.substr(5);
         console.log(textNum)
-
+        console.log('run')
         console.log($this)
         //  Then look into the textarea using that hour we've determined and pull the data out of the textarea and into a variable. THis is failing.
         addedText = $(`#text-${textNum}`).textContent
@@ -87,10 +80,10 @@ $(document).ready(function () {
         // Finally, save the text to localstorage with the  hour of the day as the key and the text as the value.
     })
 
+    // on submiting what you want the start and ending times to be, re-render the rows
     $('#hourSubmit').on('click', function(event){
         event.preventDefault();
         startingHour = $('#startHour').val()
-        console.log(startingHour)
         endingHour = $('#endingHour').val()
         renderRows(startingHour,endingHour);
     })
